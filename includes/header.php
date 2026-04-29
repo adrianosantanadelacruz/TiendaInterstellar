db0b76a0913f3042ca4f451d48d3daaad49bbb56
adrianosantanadelacruz <adridelacruzsantana@gmail.com>
2026-04-29 14:34:37 +0200

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -12,6 +12,7 @@ if (session_status() === PHP_SESSION_NONE) { session_start(); }
     <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&display=swap" rel="stylesheet">
 
     <!-- CSS PRINCIPAL (ruta fija y correcta) -->
-    <link rel="stylesheet" href="/TiendaInterstellar/public/assets/css/style.css">
+    <link rel="stylesheet" href="/assets/css/style.css?v=2">
+
 </head>
 <body>
